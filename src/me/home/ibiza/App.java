a36433ff586097a6690597be8f52c2dc70aa4477
Tamás <tarcsa.tamas90@gmail.com>
2020-04-21 11:56:13 +0200

    Problems with Miller Rabin

--- src/me/home/ibiza/App.java
+++ src/me/home/ibiza/App.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 
 import me.home.ibiza.eaAndeea.ExtendedEuclidesAlgorithm;
 import me.home.ibiza.fme.FastModularExponentation;
+import me.home.ibiza.mr.MillerRabin;
 
 public class App {
 
@@ -15,6 +16,11 @@ public class App {
 		System.out.println("Fast Modular Exponentation:");
 		FastModularExponentation fme = new FastModularExponentation(BigInteger.valueOf(5),BigInteger.valueOf(117),BigInteger.valueOf(19));
 		System.out.printf("%d on the power of %d mod %d = %d\n",5,117,19,fme.doFastModularExponentation());
+		
+		System.out.println("Miller-Rabin test:");
+		MillerRabin mr = new MillerRabin(BigInteger.valueOf(127),3);
+		System.out.println("127 is probably prime: " + mr.doMRtest() + "! tested 3 times");
+
 	}
 
 }
---
+++ src/me/home/ibiza/mr/MillerRabin.java
@@ -0,0 +1,115 @@
+package me.home.ibiza.mr;
+
+import java.math.BigInteger;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MillerRabin {
+	
+	private BigInteger n;
+	
+	private int testTime;
+
+	private BigInteger a;
+	
+	private BigInteger d;
+	
+	private BigInteger s;
+	
+	private List<BigInteger> r;
+	
+	private List<BigInteger> alreadyTestedWith;
+	
+	public MillerRabin(BigInteger n,int testTimes) {
+		if(n.remainder(BigInteger.TWO).compareTo(BigInteger.ZERO) == 0) {
+			throw new IllegalArgumentException("n cannot be odd");
+		}
+		this.n = n;
+		this.testTime = testTimes;
+		d = calculateDandSetS();
+		r = fillR();
+		alreadyTestedWith = new ArrayList<>();
+	}
+	
+	public boolean doMRtest() {
+		boolean result = true;
+		int runnedTestCases = 0;
+		while(runnedTestCases < testTime) {
+			boolean isPrime = false;
+			this.a = generateRandomA();
+			System.out.println(a + "\t" + doTestWithoutPowers() + "\t" + doTestWithPowers());
+			isPrime = isPrime || doTestWithoutPowers() || doTestWithPowers();
+			result = result && isPrime;
+			runnedTestCases++;
+		}
+		
+		return result;
+	}
+
+
+	private BigInteger generateRandomA() {
+		Random rand = new Random();
+        BigInteger min = BigInteger.TWO;
+        BigInteger center = n.subtract(min);
+        int maxNumBitLength = n.bitLength();
+        BigInteger result = new BigInteger(maxNumBitLength,rand);
+       
+        if (result.compareTo(min) < 0)
+            result = result.add(min);
+          if (result.compareTo(n) >= 0)
+            result = result.mod(center).add(min);
+        
+        
+        while(result.equals(BigInteger.ZERO) || result.equals(BigInteger.ONE) || alreadyTestedWith.contains(result)) {
+        	result = new BigInteger(n.bitCount(), rand);
+        }
+        
+        alreadyTestedWith.add(result);
+        return result;
+	}
+
+	private boolean doTestWithPowers() {
+		
+		boolean isPrime = false;
+		
+		for(BigInteger power : r) {
+			BigInteger nextMod = a.modPow(d.multiply(BigInteger.TWO.pow(power.intValue())), n);
+			isPrime = isPrime || nextMod.equals(BigInteger.ONE.negate());
+		}
+		
+		return isPrime;
+	}
+
+	private boolean doTestWithoutPowers() {
+		BigInteger baseValue = a.modPow(d, n);
+		return baseValue.equals(BigInteger.ONE);
+	}
+
+	private List<BigInteger> fillR() {
+		List<BigInteger> result = new ArrayList<>();
+
+		for(BigInteger i = BigInteger.ZERO; i.compareTo(s) < 0;i = i.add(BigInteger.ONE)) {
+			result.add(i);
+		}
+		
+		return result;
+	}
+
+
+	private BigInteger calculateDandSetS() {
+		BigInteger base = n.subtract(BigInteger.ONE);
+		BigInteger counter = BigInteger.ZERO;
+		
+		while(!base.remainder(BigInteger.TWO).equals(BigInteger.ONE)) {
+			base = base.divide(BigInteger.TWO);
+			counter = counter.add(BigInteger.ONE);
+		}
+		
+		this.s = counter;
+		return base;
+	}
+
+}
